ef8b20f83ff3865a86be45d6524afa0f06b975bd
funkydude <funkydude@users.noreply.github.com>
2008-02-13 05:39:02 +0000

    BasicMinimap: add deDE locale by Farook

---
+++ Locales/deDE.lua
@@ -0,0 +1,18 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("BasicMinimap","deDE")
+
+L["Intro"] = "BasicMinimap ist eine einfache L\195\182sung f\195\188r eine komplette, eckige Minimap. Erlaubt das Skalieren, Verschieben, Mausrad-Zoomen und Sperren der Minimap."
+
+L["Shape"] = "Format"
+L["Change the minimap shape, curcular or square."] = "\195\132ndere das Format der Minimap, rund oder quadratisch."
+L["Square"] = "Quadrat"
+L["Circular"] = "Kreis"
+
+L["Scale"] = "Skalierun"
+L["Adjust the minimap scale, from 0.5 to 2.0"] = "Die Skalierung der Minimap anpassen, von 0.5 bis 2.0"
+
+L["Strata"] = "Schicht"
+L["Change the strata of the Minimap."] = "\195\132ndere die Schicht der Minimap."
+
+L["Lock"] = "Sperren"
+L["Lock the minimap in its current location."] = "Sperrt die Minimap in der aktuellen Position."
+
