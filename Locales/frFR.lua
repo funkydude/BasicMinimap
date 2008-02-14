84aa09baf5fb8fc499e8aa49ea09ae8a0654a248
tborlee <tborlee@users.noreply.github.com>
2008-02-14 22:47:11 +0000

    BasicMinimap: - Added frFR locale

---
+++ Locales/frFR.lua
@@ -0,0 +1,19 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("BasicMinimap","frFR")
+if not L then return end
+
+L["Intro"] = "BasicMinimap est un solution basique pour une minicarte propre et carrée. Permet le redimensionnement, le déplacement, le zoom à la molette, le changement de couche et le verrouillage de la minicarte."
+
+L["Shape"] = "Forme"
+L["Change the minimap shape, curcular or square."] = "Change la forme de la minicarte : circulaire ou carrée."
+L["Square"] = "Carrée"
+L["Circular"] = "Circulaire"
+
+L["Scale"] = "Échelle"
+L["Adjust the minimap scale, from 0.5 to 2.0"] = "Modifie l'échelle de la minicarte : cela va de 0.5 à 2.0."
+
+L["Strata"] = "Couche"
+L["Change the strata of the Minimap."] = "Change la couche de la minicarte."
+
+L["Lock"] = "Verrouillage"
+L["Lock the minimap in its current location."] = "Verrouille la minicarte à sa position actuelle."
+
