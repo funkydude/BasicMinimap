2fab8f2f4a27a0e803a50db234af197a941d9339
funkydude <funkydude@users.noreply.github.com>
2009-05-16 01:11:41 +0000

    Ditch AceDB, use 1 global profile for all chars = settings reset. Sanely change scale of the minimap.

--- BasicMinimap.lua
+++ BasicMinimap.lua
@@ -34,15 +34,23 @@ local function getOptions()
 					order = 3, type = "range", width = "full",
 					min = 0.5, max = 2, step = 0.01,
 					get = function() return db.scale end,
-					set = function(_, scale) db.scale = scale _G.Minimap:SetScale(scale) end,
+					set = function(_, scale)
+						Minimap:SetScale(scale)
+						Minimap:ClearAllPoints()
+						local s = db.scale/scale
+						Minimap:SetPoint(db.point, nil, db.relpoint, db.x*s, db.y*s)
+						db.scale = scale
+						local p, _, rp, x, y = Minimap:GetPoint()
+						db.point, db.relpoint, db.x, db.y = p, rp, x, y
+					end,
 				},
 				strata = {
 					name = L["Strata"], desc = L["Change the strata of the Minimap."],
 					order = 4, type = "select", width = "full",
 					get = function() return db.strata end,
 					set = function(_, strata) db.strata = strata
-						_G.Minimap:SetFrameStrata(strata)
-						_G.BasicMinimapBorder:SetFrameStrata(strata) 
+						Minimap:SetFrameStrata(strata)
+						BasicMinimapBorder:SetFrameStrata(strata) 
 					end,
 					values = {TOOLTIP = L["Tooltip"], HIGH = _G["HIGH"], MEDIUM = _G["AUCTION_TIME_LEFT2"],
 						LOW = _G["LOW"], BACKGROUND = _G["BACKGROUND"]
@@ -54,23 +62,23 @@ local function getOptions()
 					get = function() return db.lock end,
 					set = function(_, state) db.lock = state
 						if not state then state = true else state = false end
-						_G.Minimap:SetMovable(state)
+						Minimap:SetMovable(state)
 					end,
 				},
 				shape = {
 					name = L["Shape"], desc = L["Change the minimap shape, curcular or square."],
 					order = 6, type = "select",
-					values = {square = _G["RAID_TARGET_6"], circular = _G["RAID_TARGET_2"]}, --Square, Circle
 					get = function() return db.shape end,
 					set = function(_, shape) db.shape = shape
 						if shape == "square" then
-							_G.Minimap:SetMaskTexture("Interface\\AddOns\\BasicMinimap\\Mask.blp")
+							Minimap:SetMaskTexture("Interface\\AddOns\\BasicMinimap\\Mask.blp")
 							BasicMinimapBorder:Show()
 						else
-							_G.Minimap:SetMaskTexture("Textures\\MinimapMask")
+							Minimap:SetMaskTexture("Textures\\MinimapMask")
 							BasicMinimapBorder:Hide()
 						end
 					end,
+					values = {square = _G["RAID_TARGET_6"], circular = _G["RAID_TARGET_2"]}, --Square, Circle
 				},
 				bordercolor = {
 					name = L["Border Color"], desc = L["Change the minimap border color."],
@@ -78,7 +86,7 @@ local function getOptions()
 					get = function() return db.border.r, db.border.g, db.border.b end,
 					set = function(_, r, g, b)
 						db.border.r = r db.border.g = g db.border.b = b
-						_G.BasicMinimapBorder:SetBackdropBorderColor(r, g, b)
+						BasicMinimapBorder:SetBackdropBorderColor(r, g, b)
 					end,
 					disabled = function() return db.shape ~= "square" end,
 				},
@@ -88,13 +96,13 @@ local function getOptions()
 					min = 0.5, max = 5, step = 0.5,
 					get = function() return db.borderSize end,
 					set = function(_, s) db.borderSize = s
-						_G.BasicMinimapBorder:SetBackdrop(
+						BasicMinimapBorder:SetBackdrop(
 							{edgeFile = "Interface\\Tooltips\\UI-Tooltip-Background", tile = false,
 							tileSize = 0, edgeSize = s,}
 						)
-						_G.BasicMinimapBorder:SetWidth(_G.Minimap:GetWidth()+s)
-						_G.BasicMinimapBorder:SetHeight(_G.Minimap:GetHeight()+s)
-						_G.BasicMinimapBorder:SetBackdropBorderColor(db.border.r, db.border.g, db.border.b)
+						BasicMinimapBorder:SetWidth(_G.Minimap:GetWidth()+s)
+						BasicMinimapBorder:SetHeight(_G.Minimap:GetHeight()+s)
+						BasicMinimapBorder:SetBackdropBorderColor(db.border.r, db.border.g, db.border.b)
 					end,
 					disabled = function() return db.shape ~= "square" end,
 				},
@@ -128,12 +136,14 @@ end
 
 local function kill() end
 local function Enable()
-	local defaults = {
-		profile = {
+	if not _G.BasicMinimapDB or not _G.BasicMinimapDB.scale then
+		_G.BasicMinimapDB = {
 			scale = 1.0,
-			x = nil,
-			y = nil,
-			lock = nil,
+			x = 0,
+			y = 0,
+			point = "CENTER",
+			relpoint = "CENTER",
+			lock = false,
 			shape = "square",
 			strata = "BACKGROUND",
 			border = { r = 0.73, g = 0.75, b = 1 },
@@ -141,12 +151,11 @@ local function Enable()
 			calendar = "RightButton",
 			tracking = "MiddleButton",
 		}
-	}
-	local new = LibStub("AceDB-3.0"):New("BasicMinimapDB", defaults)
-	db = new.profile
+	end
+	db = _G.BasicMinimapDB
 
-	LibStub("AceConfig-3.0"):RegisterOptionsTable("BasicMinimap", getOptions)
-	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("BasicMinimap")
+	_G.LibStub("AceConfig-3.0"):RegisterOptionsTable("BasicMinimap", getOptions)
+	_G.LibStub("AceConfigDialog-3.0"):AddToBlizOptions("BasicMinimap")
 
 	_G["SlashCmdList"]["BASICMINIMAP_MAIN"] = function() InterfaceOptionsFrame_OpenToCategory("BasicMinimap") end
 	_G["SLASH_BASICMINIMAP_MAIN1"] = "/bm"
@@ -164,12 +173,8 @@ local function Enable()
 	border:SetHeight(Minimap:GetHeight()+db.borderSize)
 	border:Hide()
 
-	if db.x and db.y then
-		Minimap:ClearAllPoints()
-		Minimap:SetPoint("CENTER", UIParent, "BOTTOMLEFT", db.x, db.y)
-	else
-		Minimap:SetPoint("CENTER", UIParent, "CENTER")
-	end
+	Minimap:ClearAllPoints()
+	Minimap:SetPoint(db.point, nil, db.relpoint, db.x, db.y)
 
 	Minimap:RegisterForDrag("LeftButton")
 	Minimap:SetClampedToScreen(true)
@@ -177,7 +182,8 @@ local function Enable()
 	Minimap:SetScript("OnDragStart", function(self) if self:IsMovable() then self:StartMoving() end end)
 	Minimap:SetScript("OnDragStop", function(self)
 		self:StopMovingOrSizing()
-		db.x, db.y = Minimap:GetCenter()
+		local p, _, rp, x, y = Minimap:GetPoint()
+		db.point, db.relpoint, db.x, db.y = p, rp, x, y
 	end)
 
 	if not db.lock then Minimap:SetMovable(true) end
