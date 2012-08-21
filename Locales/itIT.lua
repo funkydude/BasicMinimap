a3d070a18e6ba1415591b90ec918749798aa2aeb
funkydude <funkydude@users.noreply.github.com>
2012-08-21 01:55:36 +0000

    add Italian support

---
+++ Locales/itIT.lua
@@ -0,0 +1,4 @@
+
+if GetLocale() ~= "itIT" then return end
+local _, BM = ...
+--@localization(locale="itIT", namespace="Config_Module", format="lua_additive_table", handle-unlocalized="ignore", table-name="BM")@
