9dc24bd7acb2682583f7a639d61ae145a87ce59f
Kuzunkhaa <spata.marco@outlook.it>
2016-09-01 13:20:38 +0200

    Update itIT.lua

--- Locales/itIT.lua
+++ Locales/itIT.lua
@@ -1,4 +1,13 @@
 
 if GetLocale() ~= "itIT" then return end
 local _, BM = ...
+BM.AUTOZOOM = "Autoriduzione Zoom"
+BM.BORDERSIZE = "Grandezza Bordo"
+BM.BUTTONDESC = "Scegli un pulsante per aprire il Calendario e il menu Tracciamento."
+BM.CALENDAR = "Calendario"
+BM.CLASSCOLORED = "Colore di Classe"
+BM.SCALE = "Proporzione"
+BM.SHAPE = "Forma"
+BM.STRATA = "Posizione"
+BM.TOOLTIP = "Tooltip"
 
