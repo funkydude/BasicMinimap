de5f1830e025c7f6f820ae315e3802bef0829407
funkydude <funkydude@users.noreply.github.com>
2018-08-05 02:01:50 +0100

    Start work on the new database & options

--- BasicMinimap.lua
+++ BasicMinimap.lua
@@ -1,10 +1,10 @@
 
 local name = ...
+local media = LibStub("LibSharedMedia-3.0")
 
 local hideFrame = function(frame) frame:Hide() end
 local noop = function() end
 local backdrops = {}
-local db
 
 do
 	local function openOpts()
@@ -21,6 +21,7 @@ local frame = CreateFrame("Frame", name)
 frame:SetScript("OnEvent", function(f, event, ...)
 	f[event](f, event, ...)
 end)
+frame.backdrops = backdrops
 
 -- Init
 function frame:ADDON_LOADED(event, addon)
@@ -31,26 +32,32 @@ function frame:ADDON_LOADED(event, addon)
 		local defaults = {
 			profile = {
 				lock = false,
+				round = false,
+				clock = true,
+				zoneText = true,
+				missions = true,
+				raidDiffIcon = true,
+				zoomBtn = false,
+				autoZoom = true,
 				position = {"CENTER", "CENTER", 0, 0},
+				borderSize = 3,
+				scale = 1,
 				--shadow = true,
 				--outline = "NONE",
 				--font = media:GetDefault("font"),
 				colorBorder = {0,0.6,0,1},
+				calendarBtn = "RightButton",
+				trackingBtn = "MiddleButton",
 			}
 		}
 		self.db = LibStub("AceDB-3.0"):New("TempBasicMinimapDB", defaults, true)
 
-		if not BasicMinimapDB then
-			BasicMinimapDB = {
-				x = 0, y = 0,
-				point = "CENTER", relpoint = "CENTER",
-				borderR = 0, borderG = 0.6, borderB = 0
-			}
+		-- Return minimap shape for other addons
+		if not self.db.profile.round then
+			function GetMinimapShape()
+				return "SQUARE"
+			end
 		end
-		db = BasicMinimapDB
-
-		--Return minimap shape for other addons
-		if not db.round then function GetMinimapShape() return "SQUARE" end end
 	end
 end
 frame:RegisterEvent("ADDON_LOADED")
@@ -63,49 +70,61 @@ function frame:PLAYER_LOGIN(event)
 	self:CALENDAR_UPDATE_PENDING_INVITES()
 
 	local Minimap = Minimap
-	Minimap.db = db
-	Minimap.backdrops = backdrops
-
 	Minimap:SetParent(UIParent)
 	MinimapCluster:EnableMouse(false)
 
 	-- Backdrops, creating the border cleanly
 	-- Clockwise: TOP, RIGHT, BOTTOM, LEFT
-	local b = db.borderSize or 3
-	local b2 = b*2
-	local w, h = Minimap:GetWidth()+b2, Minimap:GetHeight()+b2
+	local size = self.db.profile.borderSize
+	local w, h = Minimap:GetWidth(), Minimap:GetHeight()
+	local r, b, g, a = unpack(self.db.profile.colorBorder)
 	for i = 1, 4 do
 		backdrops[i] = Minimap:CreateTexture()
-		backdrops[i]:SetColorTexture(db.borderR, db.borderG, db.borderB)
-		backdrops[i]:SetWidth(i%2==0 and b or w)
-		backdrops[i]:SetHeight(i%2==0 and h or b)
+		backdrops[i]:SetColorTexture(r, b, g, a)
+		backdrops[i]:SetWidth(i%2==0 and size or w)
+		backdrops[i]:SetHeight(i%2==0 and h or size)
 	end
 	backdrops[1]:SetPoint("BOTTOM", Minimap, "TOP")
 	backdrops[2]:SetPoint("LEFT", Minimap, "RIGHT")
 	backdrops[3]:SetPoint("TOP", Minimap, "BOTTOM")
 	backdrops[4]:SetPoint("RIGHT", Minimap, "LEFT")
+	for i = 5, 8 do
+		backdrops[i] = Minimap:CreateTexture()
+		backdrops[i]:SetColorTexture(r, b, g, a)
+		backdrops[i]:SetWidth(size)
+		backdrops[i]:SetHeight(size)
+	end
+	backdrops[5]:SetPoint("BOTTOMRIGHT", Minimap, "TOPLEFT")
+	backdrops[6]:SetPoint("BOTTOMLEFT", Minimap, "TOPRIGHT")
+	backdrops[7]:SetPoint("TOPRIGHT", Minimap, "BOTTOMLEFT")
+	backdrops[8]:SetPoint("TOPLEFT", Minimap, "BOTTOMRIGHT")
 
 	Minimap:ClearAllPoints()
-	Minimap:SetPoint(db.point, nil, db.relpoint, db.x, db.y)
+	Minimap:SetPoint(self.db.profile.position[1], UIParent, self.db.profile.position[2], self.db.profile.position[3], self.db.profile.position[4])
 	Minimap:RegisterForDrag("LeftButton")
 	Minimap:SetClampedToScreen(true)
 
 	Minimap:SetScript("OnDragStart", function(self) if self:IsMovable() then self:StartMoving() end end)
 	Minimap:SetScript("OnDragStop", function(self)
 		self:StopMovingOrSizing()
-		local p, _, rp, x, y = self:GetPoint()
-		db.point, db.relpoint, db.x, db.y = p, rp, x, y
+		local a, _, b, c, d = self:GetPoint()
+		frame.db.profile.position[1] = a
+		frame.db.profile.position[2] = b
+		frame.db.profile.position[3] = c
+		frame.db.profile.position[4] = d
 	end)
 
-	if not db.lock then Minimap:SetMovable(true) end
+	if not self.db.profile.lock then
+		Minimap:SetMovable(true)
+	end
 
-	Minimap:SetScale(db.scale or 1)
+	Minimap:SetScale(self.db.profile.scale)
 	MinimapNorthTag.Show = MinimapNorthTag.Hide
 	MinimapNorthTag:Hide()
 
 	MinimapBorder:Hide()
 	MinimapBorderTop:Hide()
-	if not db.round then
+	if not self.db.profile.round then
 		Minimap:SetMaskTexture("Interface\\BUTTONS\\WHITE8X8")
 	else
 		for i = 1, 4 do
@@ -119,17 +138,17 @@ function frame:PLAYER_LOGIN(event)
 	Minimap:SetQuestBlobRingScalar(0)
 	Minimap:SetQuestBlobRingAlpha(0)
 
-	if not db.zoomBtn then
+	if not self.db.profile.zoomBtn then
 		MinimapZoomIn:Hide()
 		MinimapZoomOut:Hide()
 	else
 		MinimapZoomIn:ClearAllPoints()
 		MinimapZoomIn:SetParent("Minimap")
-		MinimapZoomIn:SetPoint("RIGHT", "Minimap", "RIGHT", db.round and 10 or 20, db.round and -40 or -50)
+		MinimapZoomIn:SetPoint("RIGHT", "Minimap", "RIGHT", self.db.profile.round and 10 or 20, self.db.profile.round and -40 or -50)
 		MinimapZoomIn:Show()
 		MinimapZoomOut:ClearAllPoints()
 		MinimapZoomOut:SetParent("Minimap")
-		MinimapZoomOut:SetPoint("BOTTOM", "Minimap", "BOTTOM", db.round and 40 or 50, db.round and -10 or -20)
+		MinimapZoomOut:SetPoint("BOTTOM", "Minimap", "BOTTOM", self.db.profile.round and 40 or 50, self.db.profile.round and -10 or -20)
 		MinimapZoomOut:Show()
 	end
 
@@ -139,7 +158,7 @@ function frame:PLAYER_LOGIN(event)
 	local a, b = WhiteNormalNumberFont:GetFont()
 	TimeManagerClockTicker:SetFont(a, b+1, "OUTLINE")
 	TimeManagerClockButton:GetRegions():Hide()
-	if db.clock == false then
+	if not self.db.profile.clock then
 		TimeManagerClockButton:Hide()
 		TimeManagerClockButton.bmShow = TimeManagerClockButton.Show
 		TimeManagerClockButton.Show = noop
@@ -154,13 +173,13 @@ function frame:PLAYER_LOGIN(event)
 	MinimapZoneTextButton:SetPoint("BOTTOM", backdrops[1], "TOP", 0, 4)
 	local a, b = GameFontNormal:GetFont()
 	MinimapZoneText:SetFont(a, b, "OUTLINE")
-	if db.zoneText == false then
+	if not self.db.profile.zoneText then
 		MinimapZoneTextButton:Hide()
 		MinimapZoneTextButton.bmShow = MinimapZoneTextButton.Show
 		MinimapZoneTextButton.Show = noop
 	end
 
-	if db.classHall == false then
+	if not self.db.profile.missions then
 		GarrisonLandingPageMinimapButton:Hide()
 		GarrisonLandingPageMinimapButton.bmShow = GarrisonLandingPageMinimapButton.Show
 		GarrisonLandingPageMinimapButton.Show = noop
@@ -183,7 +202,7 @@ function frame:PLAYER_LOGIN(event)
 	GarrisonLandingPageMinimapButton:SetParent(Minimap)
 	GarrisonLandingPageMinimapButton:SetPoint("BOTTOMLEFT", Minimap, "BOTTOMLEFT", -23, 15)
 
-	if db.hideraid then
+	if not self.db.profile.raidDiffIcon then
 		MiniMapInstanceDifficulty:SetScript("OnShow", hideFrame)
 		MiniMapInstanceDifficulty:Hide()
 		GuildInstanceDifficulty:SetScript("OnShow", hideFrame)
@@ -209,7 +228,7 @@ function frame:PLAYER_LOGIN(event)
 	end
 
 	local zoomBtnFunc = function()
-		if db.zoom then
+		if self.db.profile.autoZoom then
 			started = started + 1
 			C_Timer.After(4, zoomOut)
 		end
@@ -228,9 +247,9 @@ function frame:PLAYER_LOGIN(event)
 	end)
 
 	Minimap:SetScript("OnMouseUp", function(self, btn)
-		if btn == (db.calendar or "RightButton") then
+		if btn == self.db.profile.calendarBtn then
 			GameTimeFrame:Click()
-		elseif btn == (db.tracking or "MiddleButton") then
+		elseif btn == self.db.profile.trackingBtn then
 			ToggleDropDownMenu(1, nil, MiniMapTrackingDropDown, self)
 		elseif btn == "LeftButton" then
 			Minimap_OnClick(self)
--- Options/Options.lua
+++ Options/Options.lua
@@ -3,8 +3,7 @@ local acr = LibStub("AceConfigRegistry-3.0")
 local acd = LibStub("AceConfigDialog-3.0")
 local media = LibStub("LibSharedMedia-3.0")
 local adbo = LibStub("AceDBOptions-3.0")
-local map = Minimap
-local db, backdrops = map.db, map.backdrops
+local map = BasicMinimap
 
 local hideFrame = function(frame) frame:Hide() end
 local noop = function() end
@@ -26,100 +25,134 @@ local buttonValues = {RightButton = KEY_BUTTON2, MiddleButton = KEY_BUTTON3,
 local acOptions = {
 	name = "BasicMinimap",
 	type = "group", childGroups = "tab",
+	get = function(info)
+		return map.db.profile[info[#info]]
+	end,
+	set = function(info, value)
+		map.db.profile[info[#info]] = value
+	end,
 	args = {
 		main = {
 			name = _G["MISCELLANEOUS"],
 			order = 1, type = "group",
 			args = {
-				btndesc = {
-					name = L.BUTTONDESC,
-					order = 1, type = "description",
-				},
-				calendarbtn = {
-					name = L.CALENDAR,
-					order = 2, type = "select",
-					get = function() return db.calendar or "RightButton" end,
-					set = function(_, btn) db.calendar = btn~="RightButton" and btn or nil end,
-					values = buttonValues,
-				},
-				trackingbtn = {
-					name = TRACKING,
-					order = 3, type = "select",
-					get = function() return db.tracking or "MiddleButton" end,
-					set = function(_, btn) db.tracking = btn~="MiddleButton" and btn or nil end,
-					values = buttonValues,
-				},
-				borderspacer = {
-					name = "\n",
-					order = 3.1, type = "description",
-				},
-				bordertitle = {
-					name = EMBLEM_BORDER, --Border
-					order = 4, type = "header",
-				},
-				bordercolor = {
+				colorBorder = {
 					name = EMBLEM_BORDER_COLOR, --Border Color
-					order = 5, type = "color",
-					get = function() return db.borderR, db.borderG, db.borderB end,
-					set = function(_, r, g, b)
-						db.borderR = r db.borderG = g db.borderB = b
-						for i = 1, 4 do
-							backdrops[i]:SetColorTexture(r, g, b)
+					order = 1, type = "color", hasAlpha = true,
+					get = function() return unpack(map.db.profile.colorBorder) end,
+					set = function(_, r, g, b, a)
+						map.db.profile.colorBorder = {r, g, b, a}
+						for i = 1, 8 do
+							map.backdrops[i]:SetColorTexture(r, g, b, a)
 						end
 					end,
-					disabled = function() return db.round end,
+					disabled = function() return map.db.profile.round end,
 				},
 				classcolor = {
 					name = L.CLASSCOLORED,
-					order = 6, type = "execute",
+					order = 2, type = "execute",
 					func = function()
 						local _, class = UnitClass("player")
 						local color = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[class] or RAID_CLASS_COLORS[class]
 						for i = 1, 4 do
-							backdrops[i]:SetColorTexture(color.r, color.g, color.b)
+							map.backdrops[i]:SetColorTexture(color.r, color.g, color.b)
 						end
-						db.borderR, db.borderG, db.borderB = color.r, color.g, color.b
+						map.db.profile.borderR, map.db.profile.borderG, map.db.profile.borderB = color.r, color.g, color.b
 					end,
-					disabled = function() return db.round end,
+					disabled = function() return map.db.profile.round end,
 				},
-				bordersize = {
+				borderSize = {
 					name = L.BORDERSIZE,
-					order = 7, type = "range", width = "full",
+					order = 3, type = "range", width = "full",
 					min = 1, max = 10, step = 1,
-					get = function() return db.borderSize or 3 end,
-					set = function(_, b) db.borderSize = b~=3 and b or nil
+					set = function(_, value)
+						map.db.profile.borderSize = value
 						-- Clockwise: TOP, RIGHT, BOTTOM, LEFT
-						local b2 = b*2
-						local w, h = Minimap:GetWidth()+b2, Minimap:GetHeight()+b2
+						local w, h = Minimap:GetWidth(), Minimap:GetHeight()
 						for i = 1, 4 do
-							backdrops[i]:SetWidth(i%2==0 and b or w)
-							backdrops[i]:SetHeight(i%2==0 and h or b)
+							map.backdrops[i]:SetWidth(i%2==0 and value or w)
+							map.backdrops[i]:SetHeight(i%2==0 and h or value)
+						end
+						for i = 5, 8 do
+							map.backdrops[i]:SetWidth(value)
+							map.backdrops[i]:SetHeight(value)
 						end
 					end,
-					disabled = function() return db.round end,
+					disabled = function() return map.db.profile.round end,
 				},
-				buttonsspacer = {
+				miscspacer = {
 					name = "\n",
-					order = 7.1, type = "description",
+					order = 4, type = "description",
 				},
-				buttonsheader = {
-					name = L.BUTTONS,
-					order = 8, type = "header",
+				mischeader = {
+					name = MISCELLANEOUS,
+					order = 5, type = "header",
 				},
-				zoom = {
-					name = ZOOM_IN.."/"..ZOOM_OUT,
+				scale = {
+					name = L.SCALE,
+					order = 6, type = "range",
+					min = 0.5, max = 2, step = 0.01,
+					set = function(_, value)
+						Minimap:SetScale(value)
+						Minimap:ClearAllPoints()
+						local s = map.db.profile.scale/value
+						map.db.profile.position[3], map.db.profile.position[4] = map.db.profile.position[3]*s, map.db.profile.position[4]*s
+						Minimap:SetPoint(map.db.profile.position[1], UIParent, map.db.profile.position[2], map.db.profile.position[3], map.db.profile.position[4])
+						map.db.profile.scale = value
+					end,
+				},
+				round = {
+					name = L.SHAPE,
 					order = 9, type = "toggle",
-					get = function() return db.zoomBtn end,
-					set = function(_, state)
-						db.zoomBtn = state
-						if state then
+					set = function(_, value)
+						map.db.profile.round = value
+						if not value then
+							Minimap:SetMaskTexture("Interface\\BUTTONS\\WHITE8X8")
+							for i = 1, 8 do
+								map.backdrops[i]:Show()
+							end
+							function GetMinimapShape() return "SQUARE" end
+						else
+							Minimap:SetMaskTexture("Interface\\AddOns\\BasicMinimap\\circle")
+							for i = 1, 8 do
+								map.backdrops[i]:Hide()
+							end
+							function GetMinimapShape() return "ROUND" end
+						end
+					end,
+				},
+				autoZoom = {
+					name = L.AUTOZOOM,
+					order = 10, type = "toggle",
+					width = "full",
+				},
+				lock = {
+					name = LOCK,
+					order = 11, type = "toggle",
+					set = function(_, value)
+						map.db.profile.lock = value
+						Minimap:SetMovable(not value)
+					end,
+				},
+			},
+		},
+		buttons = {
+			name = L.BUTTONS,
+			order = 2, type = "group",
+			args = {
+				zoomBtn = {
+					name = ZOOM_IN.."/"..ZOOM_OUT,
+					order = 1, type = "toggle",
+					set = function(_, value)
+						map.db.profile.zoomBtn = value
+						if value then
 							MinimapZoomIn:ClearAllPoints()
 							MinimapZoomIn:SetParent("Minimap")
-							MinimapZoomIn:SetPoint("RIGHT", "Minimap", "RIGHT", db.round and 10 or 20, db.round and -40 or -50)
+							MinimapZoomIn:SetPoint("RIGHT", "Minimap", "RIGHT", map.db.profile.round and 10 or 20, map.db.profile.round and -40 or -50)
 							MinimapZoomIn:Show()
 							MinimapZoomOut:ClearAllPoints()
 							MinimapZoomOut:SetParent("Minimap")
-							MinimapZoomOut:SetPoint("BOTTOM", "Minimap", "BOTTOM", db.round and 40 or 50, db.round and -10 or -20)
+							MinimapZoomOut:SetPoint("BOTTOM", "Minimap", "BOTTOM", map.db.profile.round and 40 or 50, map.db.profile.round and -10 or -20)
 							MinimapZoomOut:Show()
 						else
 							MinimapZoomIn:Hide()
@@ -127,22 +160,20 @@ local acOptions = {
 						end
 					end,
 				},
-				raiddiff = {
+				raidDiffIcon = {
 					name = RAID_DIFFICULTY,
-					order = 10, type = "toggle",
-					get = function() if db.hideraid then return false else return true end end,
-					set = function(_, state)
-						if state then
-							db.hideraid = nil
+					order = 2, type = "toggle",
+					set = function(_, value)
+						map.db.profile.raidDiffIcon = value
+						if value then
 							MiniMapInstanceDifficulty:SetScript("OnShow", nil)
 							GuildInstanceDifficulty:SetScript("OnShow", nil)
-							local z = select(2, IsInInstance())
+							local _, z = IsInInstance()
 							if z and (z == "party" or z == "raid") and (IsInRaid() or IsInGroup()) then
 								MiniMapInstanceDifficulty:Show()
 								GuildInstanceDifficulty:Show()
 							end
 						else
-							db.hideraid = true
 							MiniMapInstanceDifficulty:SetScript("OnShow", hideFrame)
 							MiniMapInstanceDifficulty:Hide()
 							GuildInstanceDifficulty:SetScript("OnShow", hideFrame)
@@ -152,11 +183,10 @@ local acOptions = {
 				},
 				clock = {
 					name = TIMEMANAGER_TITLE,
-					order = 11, type = "toggle",
-					get = function() return db.clock or db.clock == nil and true end,
-					set = function(_, state)
-						db.clock = state
-						if state then
+					order = 3, type = "toggle",
+					set = function(_, value)
+						map.db.profile.clock = value
+						if value then
 							if TimeManagerClockButton.bmShow then
 								TimeManagerClockButton.Show = TimeManagerClockButton.bmShow
 								TimeManagerClockButton.bmShow = nil
@@ -173,11 +203,10 @@ local acOptions = {
 				},
 				zoneText = {
 					name = L.ZONETEXT,
-					order = 12, type = "toggle",
-					get = function() return db.zoneText or db.zoneText == nil and true end,
-					set = function(_, state)
-						db.zoneText = state
-						if state then
+					order = 4, type = "toggle",
+					set = function(_, value)
+						map.db.profile.zoneText = value
+						if value then
 							if MinimapZoneTextButton.bmShow then
 								MinimapZoneTextButton.Show = MinimapZoneTextButton.bmShow
 								MinimapZoneTextButton.bmShow = nil
@@ -192,13 +221,12 @@ local acOptions = {
 						end
 					end,
 				},
-				classHall = {
+				missions = {
 					name = L.CLASSHALL,
-					order = 13, type = "toggle",
-					get = function() return db.classHall or db.classHall == nil and true end,
-					set = function(_, state)
-						db.classHall = state
-						if state then
+					order = 5, type = "toggle",
+					set = function(_, value)
+						map.db.profile.missions = value
+						if value then
 							if GarrisonLandingPageMinimapButton.bmShow then
 								GarrisonLandingPageMinimapButton.Show = GarrisonLandingPageMinimapButton.bmShow
 								GarrisonLandingPageMinimapButton.bmShow = nil
@@ -213,73 +241,26 @@ local acOptions = {
 						end
 					end,
 				},
-				miscspacer = {
-					name = "\n",
-					order = 13.1, type = "description",
+				clickHeaderDesc = {
+					name = "\n".. L.BUTTONDESC,
+					order = 6, type = "description",
 				},
-				mischeader = {
-					name = MISCELLANEOUS,
-					order = 14, type = "header",
+				calendarBtn = {
+					name = L.CALENDAR,
+					order = 7, type = "select",
+					values = buttonValues,
 				},
-				scale = {
-					name = L.SCALE,
-					order = 15, type = "range",
-					min = 0.5, max = 2, step = 0.01,
-					get = function() return db.scale or 1 end,
-					set = function(_, scale)
-						Minimap:SetScale(scale)
-						Minimap:ClearAllPoints()
-						local s = (db.scale or 1)/scale
-						db.x, db.y = db.x*s, db.y*s
-						Minimap:SetPoint(db.point, UIParent, db.relpoint, db.x, db.y)
-						db.scale = scale~=1 and scale or nil
-					end,
-				},
-				shape = {
-					name = L.SHAPE,
-					order = 16, type = "select",
-					get = function() return db.round and "circular" or "square" end,
-					set = function(_, shape)
-						if shape == "square" then
-							db.round = nil
-							Minimap:SetMaskTexture("Interface\\BUTTONS\\WHITE8X8")
-							for i = 1, 4 do
-								backdrops[i]:Show()
-							end
-							function GetMinimapShape() return "SQUARE" end
-						else
-							db.round = true
-							Minimap:SetMaskTexture("Interface\\AddOns\\BasicMinimap\\circle")
-							for i = 1, 4 do
-								backdrops[i]:Hide()
-							end
-							function GetMinimapShape() return "ROUND" end
-						end
-					end,
-					values = {square = RAID_TARGET_6, circular = RAID_TARGET_2}, --Square, Circle
-				},
-				autozoom = {
-					name = L.AUTOZOOM,
-					order = 17, type = "toggle",
-					width = "full",
-					get = function() return db.zoom end,
-					set = function(_, state) db.zoom = state and true or nil end,
-				},
-				lock = {
-					name = LOCK,
-					order = 18, type = "toggle",
-					get = function() return db.lock end,
-					set = function(_, state) db.lock = state and true or nil
-						if not state then state = true else state = false end
-						Minimap:SetMovable(state)
-					end,
+				trackingBtn = {
+					name = TRACKING,
+					order = 8, type = "select",
+					values = buttonValues,
 				},
 			},
 		},
 		profiles = adbo:GetOptionsTable(BasicMinimap.db),
 	},
 }
-acOptions.args.profiles.order = 2
+acOptions.args.profiles.order = 3
 
 acr:RegisterOptionsTable(acOptions.name, acOptions, true)
 acd:SetDefaultSize(acOptions.name, 430, 570)
