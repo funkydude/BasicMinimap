0a38c3d5822dcb54190300f787707935520ebf30
funkydude <funkydude@users.noreply.github.com>
2018-08-15 02:55:48 +0100

    Force the missions button to stay small since Blizz likes to resize it now.

--- BasicMinimap.lua
+++ BasicMinimap.lua
@@ -244,7 +244,10 @@ function frame:PLAYER_LOGIN(event)
 
 	-- Missions button
 	GarrisonLandingPageMinimapButton:SetParent(Minimap)
-	GarrisonLandingPageMinimapButton:SetSize(38, 38)
+	-- We now need to hook this as Blizz likes to fiddle with its size
+	hooksecurefunc(GarrisonLandingPageMinimapButton, "SetSize", function()
+		frame.SetSize(GarrisonLandingPageMinimapButton, 36, 36)
+	end)
 	if not self.db.profile.missions then
 		GarrisonLandingPageMinimapButton:SetParent(self)
 	end
